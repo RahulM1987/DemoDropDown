9698f49b2bc4ea45e5a54d31e500795bf18815c3
RahulM1987 <rahulmorade@vishal-niles-mac-mini.local>
2016-04-27 18:27:30 +0530

    Tests

--- DemoDropDown/ViewController.swift
+++ DemoDropDown/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate{
 
+    @IBOutlet weak var textV: UITextField!
+    var DataArray:NSMutableArray=["First","Second","Third","Fourth","Fifth","Sixth","Seventh","Eight"]
+
+    @IBOutlet weak var output: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        let dropdown = DropDown.init(framea: CGRectMake(textV.frame.origin.x, textV.frame.origin.y, textV.frame.width, 200), SetArray: DataArray)
+        self.view.addSubview(dropdown)
+        return false;
+    }
 }
 
